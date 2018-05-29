e7a378ea87cd01419ab34bd5ff0873bdabfb1cd5
Keith Baggerly <kabagg@gmail.com>
2018-05-29 18:18:33 -0500

    initial package creation and package documentation

---
+++ R/moffitt2018.R
@@ -0,0 +1,8 @@
+#' Infrastructure Tools Package for Moffitt 2018
+#'
+#' This package collects various scripts and templates
+#' we've found to be useful in organizing projects.
+#'
+#' @docType package
+#' @name moffitt2018
+NULL
